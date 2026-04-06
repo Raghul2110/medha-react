992089499b5b6a472317abf4f62a48c943b4a6c3
Raghul P <raghulp@medhahosting.com>
2026-04-06 17:45:58 +0530

    Created Blog page and want to create the article page and add

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,31 @@
+import Navbar from "../../components/Navbar/Navbar";
+import './Contact.css'
+
 function Contact() {
-  return <h1>Contact Page</h1>;
+  return (
+    <>
+    <Navbar></Navbar>
+    <section>
+    <div className="blog">
+      <div className="header"> <p>Home / Blog</p>
+      </div>
+      <div className="weight"><h1>Medha Cloud Blog</h1>
+        </div>
+        <span className="size"><p>Expert insights on cloud computing, Microsoft 365, Azure, cybersecurity, and </p>
+        <p>managed IT services. Guides and tutorials from certified IT professionals.</p></span>
+        <div className="size1"><pre>709+ Articles    •    14 Categories     •   Updated Weekly</pre></div> 
+    </div>
+    </section>
+
+    <section>
+      <div className="section2">
+      <div className="head"><h1>Browse by Category</h1></div>
+      <p>Explore our service-aligned blog categories — each one connected to expert services</p>
+      <p>and solutions.</p>
+      </div>
+    </section>
+    </>
+  )
 }
 
 export default Contact;
